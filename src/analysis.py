891cc259e2b2d7f9201152c75830f788f6e4abfd
emanmwendwa <emwendwa418@gmail.com>
2025-12-17 20:29:07 -0500

    initial acc code

---
+++ src/analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+races = pd.read_csv('../data/races.csv')
+drivers = pd.read_csv('../data/drivers.csv')
+constructors = pd.read_csv('../data/constructors.csv')
+circuits = pd.read_csv('../data/circuits.csv')
+results = pd.read_csv('../data/results.csv')
+seasons = pd.read_csv('../data/seasons.csv')
+sprint_results = pd.read_csv('../data/sprint_results.csv')
